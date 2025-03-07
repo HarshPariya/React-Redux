f32225cecff67505227d8f35fe53b39306afbfb6
Harsh-Pariya <harshpariya195@gmail.com>
2025-03-08 02:12:59 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Counter from './components/Counter'
+import TodoList from './components/TodList'
+import "./App.css"
 
 const App = () => {
   return (
     <div>
-      <Counter/>
+      {/* <Counter/> */}
+      <TodoList/>
     </div>
   )
 }
